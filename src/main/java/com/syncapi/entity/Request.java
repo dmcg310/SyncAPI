a3cd247dc39623cf5fa0eba5e2d3f8642958f19f
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2026-01-13 18:07:06 +0000

    fix: lint

--- src/main/java/com/syncapi/entity/Request.java
+++ src/main/java/com/syncapi/entity/Request.java
@@ -1,6 +1,16 @@
 package com.syncapi.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
--- src/main/java/com/syncapi/entity/Workspace.java
+++ src/main/java/com/syncapi/entity/Workspace.java
@@ -1,6 +1,17 @@
 package com.syncapi.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
