58c06e4ddbfd60a31c716da2deb363a484e033ef
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2026-02-02 14:35:35 +0000

    feat: frontend setup

---
+++ frontend/src/util/constants.js
@@ -0,0 +1,22 @@
+export const API_BASE_URL = '/api';
+
+export const HTTP_METHODS = {
+    GET: 'GET',
+    POST: 'POST',
+    PUT: 'PUT',
+    PATCH: 'PATCH',
+    DELETE: 'DELETE'
+};
+
+export const METHOD_COLORS = {
+    GET: 'bg-http-get',
+    POST: 'bg-http-post',
+    PUT: 'bg-http-put',
+    PATCH: 'bg-http-patch',
+    DELETE: 'bg-http-delete'
+};
+
+export const STORAGE_KEYS = {
+    TOKEN: 'syncapi_token',
+    USER: 'syncapi_user'
+};
